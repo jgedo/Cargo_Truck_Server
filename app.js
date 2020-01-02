5421e87d0a93078a3bd173e7690573e80beeb596
jgedo <jge1591@gmail.com>
2020-01-02 16:48:16 -0600

    made POST test method to insert records in SNOW

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 require('dotenv/config');
 
 // Connect to database
@@ -25,10 +26,26 @@ const trailersRoute = require('./routes/trailers');
 //app.use('/posts', postsRoute);
 app.use('/api/trailers', trailersRoute);
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('We are on home');
+app.get('/testSnow', async (req, res) => {
+    //const trailer = await Trailer.findById(req.params.id);
+    axios.post('https://dev85450.service-now.com/api/422579/testtrailer',{
+        manufacturer:"ddemo manufacturer",
+        model:"dnasjkdbn"
+    }, {
+        headers: { Authorization: "Basic cmVzdC50ZXN0aW5nOmFkbWluMTEx" }
+    })
+    .then((res) => {
+        res.json(res);
+    })
+    .catch((error) => {
+        res.json(error);
+    });
 });
+
+// Routes
+/*app.get('/', (req, res) => {
+    res.send('We are on home');
+});*/
 
 // Listen
 app.listen(3000);
